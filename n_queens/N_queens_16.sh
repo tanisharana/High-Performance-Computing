ebad936a0013a1cabd76b9a688b194eae452e90f
tanisharana <tanisha.rana1109@gmail.com>
2023-02-17 11:54:23 +0530

    n queens with varying board sizes

---
+++ n_queens/N_queens_16.sh
@@ -0,0 +1,28 @@
+set logscale x 2
+unset label
+#set key above
+#set key outside top center horizontal font ",3"
+#set autoscale
+#set style line 1 linetype 2
+set term pdf size 9,4
+set output "N_queens_16.pdf" 
+set xtics (1,2,4,6,8,10,12,14,16) font "Bold,7" offset 0, graph 0.05
+set ytics font "Bold,7"
+#set size ratio 0.9
+set xlabel "No of Threads" font "Bold,9" offset 0,1.25
+set ylabel "Run time (s)" font "Bold,9" offset 3,0
+#set key box
+#set key outside horizontal font ",3"
+#unset tics
+set key at screen 0.6,0.61 font ",7" vertical sample 0.4 spacing 0.3 width 0.5 height 1.0 maxrows 1
+#set key off
+#set key on center top horizontal center samplen 0.3 spacing 0.5 width 0.3 height 0.3  box lw 1 font ",3"
+#set key at 8,16
+set multiplot layout 1,2
+set yrange [320: 350]
+#set ylabel "Run time (ms)" font ",3"
+set title "N queens, 16 Boardsize" font "Bold,10" offset -1.0,-3.5
+set size 0.4,0.7
+plot "N_queens.dat" using 1:2 notitle with linespoint lw 2 ps 0.25,\
+
+
---
+++ n_queens/n_queens_12.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define N 12
+
+int count = 0;
+int board[N][N];
+
+void print_board()
+{
+    int i, j;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++)
+            printf("%d\t", board[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int is_attack(int row, int col)
+{
+    int i, j;
+
+    for (i = 0; i < col; i++)
+        if (board[row][i])
+            return 1;
+
+    for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+        if (board[i][j])
+            return 1;
+
+    for (i = row, j = col; j >= 0 && i < N; i++, j--)
+        if (board[i][j])
+            return 1;
+
+    return 0;
+}
+
+int nqueens(int col)
+{
+    int i;
+
+    if (col == N) {
+#pragma omp critical
+        count++;
+        return 1;
+    }
+
+    for (i = 0; i < N; i++) {
+        if (!is_attack(i, col)) {
+            board[i][col] = 1;
+            nqueens(col + 1);
+            board[i][col] = 0;
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    int i, j;
+    double start, end;
+    FILE *fp;
+
+    fp = fopen("nqueens_time.dat", "w");
+    if (fp == NULL) {
+        printf("Unable to open file\n");
+        return 1;
+    }
+
+    for (i = 1; i <= 16; i++) {
+        count = 0;
+        omp_set_num_threads(i);
+        start = omp_get_wtime();
+
+        #pragma omp parallel
+        {
+            #pragma omp single
+            nqueens(0);
+        }
+
+        end = omp_get_wtime();
+
+        printf("Number of threads: %d\n", i);
+        printf("Number of solutions: %d\n", count);
+        printf("Time taken: %g\n", end - start);
+        fprintf(fp, "%d %g\n", i, end - start);
+    }
+
+    fclose(fp);
+    return 0;
+}
